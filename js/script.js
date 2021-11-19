779810793b6321a8124ef9eca23a32a69efe9dff
tatsunori <iwai.tatsunori1126@gmail.com>
2021-11-20 06:16:33 +0900

    2021.11.20_アニメーション完成

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 $(function () {
-    $('.js-btn').on('click', function () {        // js-btnクラスをクリックすると、
-      $('.btn-line').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
-      $('#slide-menu').slideToggle(200);
-    })
+  $(document).ready(function() {
+    $('.p-main__top-visual-title').fadeIn(2000);
+  });
+
+  $('.js-btn').on('click', function () {        // js-btnクラスをクリックすると、
+    $('.btn-line').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
+    $('#slide-menu').slideToggle(200);
+  })
 });
